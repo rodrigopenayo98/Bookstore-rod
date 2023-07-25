70ea389a2bd7a848e4ee4d8993ab030a302bc2b6
rod <91923422+rodrigopenayo98@users.noreply.github.com>
2023-07-25 15:23:10 -0300

    use redux in react components

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,14 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../redux/books/booksSlice';
+import Button from './Button';
 
-const BookForm = () => (
-  <div>
-    <h1>ADD NEW BOOK</h1>
-    <form>
-      <input type="text" id="InputNameBook" placeholder="Book Title" />
-      <input type="text" id="InputNameAuthor" placeholder="Author" />
-      <button type="submit" id="BookFormButton">Add Book</button>
-    </form>
-  </div>
-);
+const AddBook = () => {
+  const dispatch = useDispatch();
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
 
-export default BookForm;
+  const handleAddBook = () => {
+    const newItemId = `item${Math.random().toString(36).substr(2, 9)}`;
+
+    dispatch(addBook({
+      item_id: newItemId,
+      title,
+      author,
+      category: 'Fiction',
+    }));
+
+    setTitle('');
+    setAuthor('');
+  };
+
+  return (
+    <div>
+      <h1>ADD NEW BOOK</h1>
+      <form>
+        <input
+          type="text"
+          placeholder="Book Title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Author"
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        />
+        <Button text="Add Book" handleAddBook={handleAddBook} />
+      </form>
+    </div>
+  );
+};
+
+export default AddBook;
