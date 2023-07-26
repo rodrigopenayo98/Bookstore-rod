b9d6d3611dca28bb1cb3cff21747076b4f091c7b
rod <91923422+rodrigopenayo98@users.noreply.github.com>
2023-07-26 14:50:46 -0300

    connect to Bookstore API

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,25 +1,56 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { addBook } from '../redux/books/booksSlice';
+import { addBook, removeBook } from '../redux/books/booksSlice';
+import api from './Api';
 import Button from './Button';
 
-const AddBook = () => {
+const BookForm = () => {
   const dispatch = useDispatch();
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
+  const [books, setBooks] = useState([]);
 
-  const handleAddBook = () => {
+  useEffect(() => {
+    const fetchBooks = async () => {
+      try {
+        const response = await api.get('/books');
+        setBooks(response.data);
+      } catch (error) {
+        console.error('Error fetching books:', error.message);
+      }
+    };
+
+    fetchBooks();
+  }, []);
+
+  const handleAddBook = async () => {
     const newItemId = `item${Math.random().toString(36).substr(2, 9)}`;
 
-    dispatch(addBook({
+    const newBook = {
       item_id: newItemId,
       title,
       author,
       category: 'Fiction',
-    }));
+    };
 
-    setTitle('');
-    setAuthor('');
+    try {
+      await api.post('/books', newBook);
+      dispatch(addBook(newBook));
+
+      setTitle('');
+      setAuthor('');
+    } catch (error) {
+      console.error('Error adding book:', error.message);
+    }
+  };
+
+  const handleRemoveBook = async (bookId) => {
+    try {
+      await api.delete(`/books/${bookId}`);
+      dispatch(removeBook(bookId));
+    } catch (error) {
+      console.error('Error removing book:', error.message);
+    }
   };
 
   return (
@@ -40,8 +71,24 @@ const AddBook = () => {
         />
         <Button text="Add Book" handleAddBook={handleAddBook} />
       </form>
+
+      <div>
+        <h2>Books List</h2>
+        <ul>
+          {Array.isArray(books) && books.map((book) => (
+            <li key={book.item_id}>
+              {book.title}
+              {' '}
+              -
+              {book.author}
+              {' '}
+              <Button text="Remove" handleOnClick={() => handleRemoveBook(book.item_id)} />
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
 
-export default AddBook;
+export default BookForm;
